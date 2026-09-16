a99c5c6333373dad92c4acf8af53fe6f725f7d33
ExportMosh <ex.cat@moshecom.com>
2026-09-16 22:46:13 +0530

    Create do-logout.js

---
+++ functions/do-logout.js
@@ -0,0 +1,8 @@
+import { clearCookieHeader } from './_utils.js';
+
+export async function onRequestPost(context) {
+  return new Response(JSON.stringify({ ok: true }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json', 'Set-Cookie': clearCookieHeader() }
+  });
+}
